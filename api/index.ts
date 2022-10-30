28230ff5b38ed28065ae78a777a12d25456fabd4
minegrass <yapxh2004@hotmail.com>
2022-10-30 22:14:54 +0800

    rename routes to api
    following vercel rules

--- api/index.ts
+++ api/index.ts
@@ -1,24 +1,10 @@
 import express from "express";
-import cors from "cors";
-import cookiePraser from "cookie-parser";
-import { config } from "dotenv";
-import jwt from "jsonwebtoken";
-import { dcLogin, dcListenTakeOrder } from "../discordBot";
-config();
 const app = express();
-const port = process.env.PORT || 3001;
-app.use(express.json());
-app.use(cors());
-app.use(cookiePraser());
-//routes
+const router = express.Router();
 
-app.use("/sendorder", require("../routes/sendOrder"));
-app.use("/", require("../routes/index"));
-
-app.listen(port, () => {
-  console.log(`server running on ${port}`);
+// getting order data
+router.get("/", async (req, res) => {
+  res.send("OK!");
 });
 
-app.on("uncaughtException", (err) => {
-  console.log(err);
-});
+module.exports = router;
